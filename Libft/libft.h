ccaa5a16c165c332a4768c4fb1a79b7f0fe2ed06
David Filipe Aguiar Pinto <daguiar-@c1r6s6.42lisboa.com>
2022-10-25 15:38:22 +0100

    fc

---
+++ Libft/libft.h
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daguiar- <daguiar-@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/24 15:25:51 by daguiar-          #+#    #+#             */
+/*   Updated: 2022/10/24 19:04:04 by daguiar-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+int		ft_isalnum(int c);
+
+int		ft_isalpha(int c);
+
+int		ft_isdigit(int c);
+
+int		ft_isascii(int c);
+
+int		ft_isprint(int c);
+
+size_t	ft_strlen(const char *s);
+
+void	*ft_memset(void *s, int c, size_t n);
+
+void	*ft_bzero(void *s, size_t n);
+
+int		ft_tolower(int c);
+
+int		ft_toupper(int c);
+
+char	*strrchr(const char *s, int c);
+
+char	*strchr(const char *s, int c);
+
+#endif
