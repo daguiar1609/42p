121977ce71393f9a02267077c66bde3b66aad4e4
David Pinto <daguiar-@student.42lisboa.com>
2023-02-10 18:19:05 +0000

    added verifs

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: daguiar- <daguiar-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 16:52:48 by daguiar-          #+#    #+#             */
-/*   Updated: 2023/02/10 16:51:36 by daguiar-         ###   ########.fr       */
+/*   Updated: 2023/02/10 18:03:52 by daguiar-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,21 @@
 
 char	*get_next_line(int fd)
 {
-	static char	*save;
-	char		buff[BUFFER_SIZE + 1];
+	static char	buff[BUFFER_SIZE + 1];
+	char		*save;
+	int			i;
 
-	if (read(fd, 0, 0) < 0)
+	i = 0;
+	if (BUFFER_SIZE < 1 || read(fd, 0, 0) < 0)
 	{
+		while (buff[i])
+			buff[i++] = 0;
 		return (NULL);
 	}
-
-	while (read(fd, buff, BUFFER_SIZE))
+	save = NULL;
+	while (read(fd, buff, BUFFER_SIZE) > 0)
 	{
 		save = ft_strjoin(save, buff);
 	}
-
-	free (buff);
 	return (save);
 }
